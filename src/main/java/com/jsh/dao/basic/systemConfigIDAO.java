df49f5a3508a48a276b1e9b14404a4125e6177c9
季圣华 <752718920@qq.com>
2017-10-05 16:46:50 +0800

    增加系统配置的功能(调整)

--- src/main/java/com/jsh/dao/basic/systemConfigIDAO.java
+++
@@ -1,9 +0,0 @@
-package com.jsh.dao.basic;
-
-import com.jsh.base.BaseIDAO;
-import com.jsh.model.po.SystemConfig;
-
-public interface SystemConfigIDAO extends BaseIDAO<SystemConfig>
-{
-	
-}
--- src/main/java/com/jsh/service/basic/systemConfigIService.java
+++
@@ -1,9 +0,0 @@
-package com.jsh.service.basic;
-
-import com.jsh.base.BaseIService;
-import com.jsh.model.po.SystemConfig;
-
-public interface SystemConfigIService extends BaseIService<SystemConfig>
-{
-	
-}
