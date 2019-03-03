31e8756b8bfd906deee602eeaf557c2bffc5bac0
季圣华 <852955+jishenghua@users.noreply.gitee.com>
2019-03-03 13:28:21 +0800

    增加页面过滤器，提高系统安全性

--- src/main/java/com/jsh/erp/ErpApplication.java
+++ src/main/java/com/jsh/erp/ErpApplication.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.jsh.erp.datasource.mappers"})
+@ServletComponentScan
 @EnableScheduling
 public class ErpApplication{
     public static void main(String[] args) {
