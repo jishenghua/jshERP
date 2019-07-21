65df87cb0a166ef84684beff2e1054bff6e7bd55
季圣华 <752718920@qq.com>
2019-07-21 20:44:53 +0800

    解决租户模块资源引用的bug

--- src/main/java/com/jsh/erp/service/tenant/TenantComponent.java
+++ src/main/java/com/jsh/erp/service/tenant/TenantComponent.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service(value = "tenant_component")
-@UserResource
+@TenantResource
 public class TenantComponent implements ICommonQuery {
 
     @Resource
