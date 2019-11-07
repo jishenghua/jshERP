4d613006a320009790a11a009b665e691ce6b065
季圣华 <752718920@qq.com>
2019-11-07 10:11:58 +0800

    增加统计模块

--- erp_web/js/common/common.js
+++ erp_web/js/common/common.js
@@ -1,3 +1,12 @@
+	//baidu-tongji
+	var _hmt = _hmt || [];
+	(function() {
+		var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?1cd9bcbaae133f03a6eb19da6579aaba";
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})();
+
 	$.fn.serializeObject = function() {
 		var o = {};
 		var a = this.serializeArray();
