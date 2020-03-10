d74682553338cb5e68ecc053e77864f127e49a83
季圣华 <752718920@qq.com>
2020-03-10 19:52:42 +0800

    优化首页

--- erp_web/js/modules/sys/sysIndex.js
+++ erp_web/js/modules/sys/sysIndex.js
@@ -418,28 +418,8 @@ $(function () {
         });
     });
 
-    toastr.options = {
-        "closeButton": true,
-        "debug": true,
-        "positionClass": "toast-bottom-right",
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "10000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-    };
-
-    //消息弹窗
-    var loginName = sessionStorage.getItem("loginName");
-    if(loginName == "jsh") {
-        toastr.info('您当前正在使用演示账号，<br/>如需正式使用请注册 <a href="/register.html"><b>点击注册</b></a>');
-    }
-
     //广告循环
+    var loginName = sessionStorage.getItem("loginName");
     if(loginName == "jsh") {
         //根据时间戳决定展示aliyun还是tencent
         function autoChangeTip() {
