f8ee507ad7de22f61cf18f73a12ff68ff08b0ab6
季圣华 <752718920@qq.com>
2021-01-27 10:42:00 +0800

    增加ajax全局时间戳的配置

--- erp_web/js/common/common.js
+++ erp_web/js/common/common.js
@@ -7,6 +7,15 @@
 		s.parentNode.insertBefore(hm, s);
 	})();
 
+	//全局配置
+	$.ajaxSetup({
+		beforeSend: function(jqXHR, settings) {
+			//在请求前给修改url（增加一个时间戳参数）
+			settings.url += settings.url.match(/\?/) ? "&" : "?";
+			settings.url += "t=" + new Date().getTime();
+		}
+	});
+
 	$.fn.serializeObject = function() {
 		var o = {};
 		var a = this.serializeArray();
