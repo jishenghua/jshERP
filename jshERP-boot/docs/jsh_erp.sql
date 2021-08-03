641d5c5cc2182caff6e54b806da6d45b14350ef1
季圣华 <752718920@qq.com>
2021-08-03 23:38:00 +0800

    更新sql

--- jshERP-boot/docs/jsh_erp.sql
+++ jshERP-boot/docs/jsh_erp.sql
@@ -685,7 +685,7 @@ CREATE TABLE `jsh_platform_config` (
 -- Records of jsh_platform_config
 -- ----------------------------
 INSERT INTO `jsh_platform_config` VALUES ('1', 'platform_name', '平台名称', '华夏ERP');
-INSERT INTO `jsh_platform_config` VALUES ('2', 'activation_code', '激活码', 'b687998cc991278832e0ebcb7a6e7d0f6870c29d21679e58fa91e388985b9df5c2cd553a8d2f91de');
+INSERT INTO `jsh_platform_config` VALUES ('2', 'activation_code', '激活码', 'e20cc75acddfe869428bcb8ce5e37b161b2490c81c178fba384938e5b3df5fbac2cd553a8d2f91de');
 INSERT INTO `jsh_platform_config` VALUES ('3', 'platform_url', '官方网站', 'http://www.huaxiaerp.com/');
 
 -- ----------------------------
