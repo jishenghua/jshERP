ad66a0e72c672dd13da86188e68aab97546b535f
KagurazakaNyaa <i@kagurazakanyaa.com>
2021-11-08 09:22:13 +0800

    增加构建缓存

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t mvn_builder -f mvn_builder.dockerfile .
-docker run --rm -v $(pwd)/jshERP-boot:/app/src -v /var/run/docker.sock:/var/run/docker.sock -w /app/src mvn_builder mvn -T 2C clean compile jib:dockerBuild
+docker run --rm -v $(pwd)/jshERP-boot:/app/src -v $(pwd)/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock -w /app/src mvn_builder mvn -T 2C clean compile jib:dockerBuild
 
 docker-compose build --parallel
 docker-compose up -d
