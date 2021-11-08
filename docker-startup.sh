89dcbf4ef04454ce2290b45175ac59e0df697b11
KagurazakaNyaa <i@kagurazakanyaa.com>
2021-11-08 09:15:56 +0800

    构建宿主机不再需要安装maven

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd jshERP-boot
-mvn clean compile jib:dockerBuild
+docker build -t mvn_builder -f mvn_builder.dockerfile .
+docker run --rm -v $(pwd)/jshERP-boot:/app/src -v /var/run/docker.sock:/var/run/docker.sock -w /app/src mvn_builder mvn -T 2C clean compile jib:dockerBuild
 
-cd ..
 docker-compose build --parallel
 docker-compose up -d
