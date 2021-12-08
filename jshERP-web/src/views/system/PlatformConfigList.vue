69a77358e67fec846438632dcd54e79145ce3196
季圣华 <752718920@qq.com>
2021-12-09 00:44:10 +0800

    增加平台配置的页面

---
+++ jshERP-web/src/views/system/PlatformConfigList.vue
@@ -0,0 +1,103 @@
+<template>
+  <a-row :gutter="24">
+    <a-col :md="24">
+      <a-card :style="cardStyle" :bordered="false">
+        <!-- table区域-begin -->
+        <div>
+          <a-table
+            ref="table"
+            size="middle"
+            bordered
+            rowKey="id"
+            :columns="columns"
+            :dataSource="dataSource"
+            :pagination="ipagination"
+            :scroll="scroll"
+            :loading="loading"
+            @change="handleTableChange">
+            <span slot="action" slot-scope="text, record">
+              <a @click="handleEdit(record)">编辑</a>
+            </span>
+          </a-table>
+        </div>
+        <!-- table区域-end -->
+        <!-- 表单区域 -->
+        <platform-config-modal ref="modalForm" @ok="modalFormOk"></platform-config-modal>
+      </a-card>
+    </a-col>
+  </a-row>
+</template>
+<!-- f r o m 7 5  2 7 1  8 9 2 0 -->
+<script>
+  import PlatformConfigModal from './modules/PlatformConfigModal'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  export default {
+    name: "PlatformConfigList",
+    mixins:[JeecgListMixin],
+    components: {
+      PlatformConfigModal
+    },
+    data () {
+      return {
+        currentRoleId: '',
+        labelCol: {
+          span: 5
+        },
+        wrapperCol: {
+          span: 18,
+          offset: 1
+        },
+        // 查询条件
+        queryParam: {platformKey:'',},
+        // 表头
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key:'rowIndex',
+            width:40,
+            align:"center",
+            customRender:function (t,r,index) {
+              return parseInt(index)+1;
+            }
+          },
+          {
+            title: '配置名称',
+            dataIndex: 'platformKeyInfo',
+            width: 100
+          },
+          {
+            title: '配置值',
+            dataIndex: 'platformValue',
+            width: 500
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            width: 100,
+            scopedSlots: { customRender: 'action' },
+          }
+        ],
+        url: {
+          list: "/platformConfig/list",
+          delete: "/platformConfig/delete",
+          deleteBatch: "/platformConfig/deleteBatch"
+        },
+      }
+    },
+    methods: {
+      handleEdit: function (record) {
+        this.$refs.modalForm.edit(record);
+        this.$refs.modalForm.title = "编辑";
+        this.$refs.modalForm.disableSubmit = false;
+        if(this.btnEnableList.indexOf(1)===-1) {
+          this.$refs.modalForm.isReadOnly = true
+        }
+      }
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less'
+</style>
---
+++ jshERP-web/src/views/system/modules/PlatformConfigModal.vue
@@ -0,0 +1,100 @@
+<template>
+  <a-modal
+    :title="title"
+    :width="800"
+    :visible="visible"
+    :confirmLoading="confirmLoading"
+    @ok="handleOk"
+    @cancel="handleCancel"
+    cancelText="关闭"
+    wrapClassName="ant-modal-cust-warp"
+    style="top:30%;height: 40%;overflow-y: hidden">
+    <a-spin :spinning="confirmLoading">
+      <a-form :form="form" id="platformConfigModal">
+        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="配置名称">
+          <a-input placeholder="请输入配置名称" v-decorator.trim="[ 'platformKeyInfo' ]" :readOnly="true" />
+        </a-form-item>
+        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="配置值">
+          <a-input placeholder="请输入配置值" v-decorator.trim="[ 'platformValue' ]" />
+        </a-form-item>
+      </a-form>
+    </a-spin>
+  </a-modal>
+</template>
+<script>
+  import pick from 'lodash.pick'
+  import {addPlatformConfig,editPlatformConfig } from '@/api/api'
+  import {autoJumpNextInput} from "@/utils/util"
+  export default {
+    name: "PlatformConfigModal",
+    data () {
+      return {
+        title:"操作",
+        visible: false,
+        model: {},
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        form: this.$form.createForm(this)
+      }
+    },
+    created () {
+    },
+    methods: {
+      add () {
+        this.edit({});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'platformKeyInfo', 'platformValue'))
+          autoJumpNextInput('platformConfigModal')
+        });
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let formData = Object.assign(this.model, values);
+            let obj;
+            if(!this.model.id){
+              obj=addPlatformConfig(formData);
+            }else{
+              obj=editPlatformConfig(formData);
+            }
+            obj.then((res)=>{
+              if(res.code === 200){
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.data.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+              that.close();
+            })
+          }
+        })
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
