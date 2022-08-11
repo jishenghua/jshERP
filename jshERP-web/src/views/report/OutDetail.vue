c0855b29df11c35be0928ed43c506382f768494d
季圣华 <752718920@qq.com>
2022-08-11 23:57:40 +0800

    优化出库明细页面的传参

--- jshERP-web/src/views/report/OutDetail.vue
+++ jshERP-web/src/views/report/OutDetail.vue
@@ -151,7 +151,6 @@
           endTime: moment().format('YYYY-MM-DD'),
           roleType: Vue.ls.get('roleType'),
           type: "出库",
-          subType: "销售",
           remark: ''
         },
         ipagination:{
