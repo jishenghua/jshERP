ecc32e0bc6365b16a0d5fbe083d4c918fee434b4
季圣华 <752718920@qq.com>
2022-09-09 01:26:57 +0800

    给组调拨单界面去掉序列号和批号的选择

--- jshERP-web/src/views/bill/modules/AllocationOutModal.vue
+++ jshERP-web/src/views/bill/modules/AllocationOutModal.vue
@@ -151,9 +151,6 @@
             { title: '库存', key: 'stock', width: '5%', type: FormTypes.normal },
             { title: '调入仓库', key: 'anotherDepotId', width: '7%', type: FormTypes.select, placeholder: '请选择${title}', options: [], allowSearch:true},
             { title: '单位', key: 'unit', width: '4%', type: FormTypes.normal },
-            { title: '序列号', key: 'snList', width: '12%', type: FormTypes.popupJsh, kind: 'sn', multi: true },
-            { title: '批号', key: 'batchNumber', width: '7%', type: FormTypes.popupJsh, kind: 'batch', multi: false },
-            { title: '有效期', key: 'expirationDate',width: '7%', type: FormTypes.normal },
             { title: '多属性', key: 'sku', width: '9%', type: FormTypes.normal },
             { title: '数量', key: 'operNumber', width: '5%', type: FormTypes.inputNumber, statistics: true,
               validateRules: [{ required: true, message: '${title}不能为空' }]
