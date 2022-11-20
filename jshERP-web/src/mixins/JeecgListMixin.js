86a787992a5d450b703095b42fada4a9562a3e87
季圣华 <752718920@qq.com>
2022-11-20 12:24:35 +0800

    优化客户和供应商对账单，扣除退货单的欠款金额

--- jshERP-web/src/mixins/JeecgListMixin.js
+++ jshERP-web/src/mixins/JeecgListMixin.js
@@ -403,7 +403,7 @@ export const JeecgListMixin = {
         let parseCols = 'initialStock,currentStock,currentStockPrice,initialAmount,thisMonthAmount,currentAmount,inSum,inSumPrice,' +
           'inOutSumPrice,outSum,outSumPrice,outInSumPrice,operNumber,allPrice,numSum,priceSum,prevSum,thisSum,thisAllPrice,changeAmount,' +
           'allPrice,taxMoney,currentNumber,lowSafeStock,highSafeStock,lowCritical,highCritical,preNeed,debtMoney,backMoney,allNeed,' +
-          'needDebt,finishDebt,debt'
+          'needDebt,realNeedDebt,finishDebt,debt'
         columns.forEach(column => {
           let { key, dataIndex } = column
           if (![key, dataIndex].includes(numKey)) {
