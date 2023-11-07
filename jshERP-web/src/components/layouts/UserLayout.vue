17926f560fa0620aaa0d6bcd2ed47fc491bd08d8
季圣华 <752718920@qq.com>
2023-11-07 22:21:57 +0800

    给登录界面增加提示

--- jshERP-web/src/components/layouts/UserLayout.vue
+++ jshERP-web/src/components/layouts/UserLayout.vue
@@ -63,6 +63,7 @@
         <a-button key="back" @click="handleMiniProgramCancel">取消</a-button>
       </template>
       <div class="platform-modal"><img src="/static/weixin-code.png" style="width:200px;" /></div>
+      <div style="text-align:center"><b>管伊佳ERP</b>为华夏ERP旗下产品</div>
     </a-modal>
   </div>
 </template>
