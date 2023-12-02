3649b9a5d115a0c0c25a7fffa3dbd65c1ea82f5a
季圣华 <752718920@qq.com>
2023-12-02 22:33:01 +0800

    把插件的菜单加粗改为不加粗

--- jshERP-web/src/components/menu/index.js
+++ jshERP-web/src/components/menu/index.js
@@ -120,25 +120,14 @@ export default {
           item.meta = Object.assign(item.meta, { hidden: true })
         })
       }
-      if(menu.component==='/layouts/IframePageView') {
-        return (
-          <Item {...{ key: menu.url }}>
-            <tag {...{ props, attrs }}>
-              {this.renderIcon(menu.icon)}
-              <span><b>{menu.text}</b></span>
-            </tag>
-          </Item>
-        )
-      } else {
-        return (
-          <Item {...{ key: menu.url }}>
-            <tag {...{ props, attrs }}>
-              {this.renderIcon(menu.icon)}
-              <span>{menu.text}</span>
-            </tag>
-          </Item>
-        )
-      }
+      return (
+        <Item {...{ key: menu.url }}>
+          <tag {...{ props, attrs }}>
+            {this.renderIcon(menu.icon)}
+            <span>{menu.text}</span>
+          </tag>
+        </Item>
+      )
     },
     renderSubMenu (menu) {
       const itemArr = []
