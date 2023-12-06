4c08890e768889729268419e40b7d8d75919ee08
季圣华 <752718920@qq.com>
2023-12-06 22:53:30 +0800

    修改系统配置更新的接口逻辑

--- jshERP-boot/src/main/java/com/jsh/erp/service/systemConfig/SystemConfigService.java
+++ jshERP-boot/src/main/java/com/jsh/erp/service/systemConfig/SystemConfigService.java
@@ -107,7 +107,7 @@ public class SystemConfigService {
         int result=0;
         try{
             result=systemConfigMapper.insertSelective(systemConfig);
-            logService.insertLog("系统配置",
+            logService.insertLogWithUserId(userService.getCurrentUser().getId(), userService.getCurrentUser().getTenantId(), "系统配置",
                     new StringBuffer(BusinessConstants.LOG_OPERATION_TYPE_ADD).append(systemConfig.getCompanyName()).toString(), request);
         }catch(Exception e){
             JshException.writeFail(logger, e);
@@ -121,7 +121,7 @@ public class SystemConfigService {
         int result=0;
         try{
             result = systemConfigMapper.updateByPrimaryKeySelective(systemConfig);
-            logService.insertLog("系统配置",
+            logService.insertLogWithUserId(userService.getCurrentUser().getId(), userService.getCurrentUser().getTenantId(), "系统配置",
                     new StringBuffer(BusinessConstants.LOG_OPERATION_TYPE_EDIT).append(systemConfig.getCompanyName()).toString(), request);
         }catch(Exception e){
             JshException.writeFail(logger, e);
