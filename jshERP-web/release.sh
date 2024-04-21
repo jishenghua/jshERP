c937e8c1b8199d5629313e5a16043a50869a555b
liminghui <766249680@qq.com>
2024-04-21 14:08:20 +0800

    feat: 前端自动化测试

---
+++ jshERP-web/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+# 进入指定目录
+cd /root/release/jshERP
+
+# 更新代码
+git fetch --all && git reset --hard origin/master
+
+cd ./jshERP-web
+
+# 更新与构建项目
+yarn install
+yarn build
+
+# 构建 Docker 镜像
+tag=$(date "+%Y%m%d%H%M")
+docker build -f Dockerfile -t release-erp-demo:$tag .
+
+# 删除旧容器并启动新容器
+docker stop release-erp-demo && sudo docker rm release-erp-demo
+docker run -p 84:84 -d --name release-erp-demo release-erp-demo:$tag
