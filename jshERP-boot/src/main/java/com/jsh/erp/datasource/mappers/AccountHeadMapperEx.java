1b0bffbaffbee83819d0fed2410d7d00a3d93223
jishenghua <752718920@qq.com>
2025-02-24 23:47:19 +0800

    给单据补回count方法

--- jshERP-boot/src/main/java/com/jsh/erp/datasource/mappers/AccountHeadMapperEx.java
+++ jshERP-boot/src/main/java/com/jsh/erp/datasource/mappers/AccountHeadMapperEx.java
@@ -26,6 +26,20 @@ public interface AccountHeadMapperEx {
             @Param("remark") String remark,
             @Param("number") String number);
 
+    Long countsByAccountHead(
+            @Param("type") String type,
+            @Param("creatorArray") String[] creatorArray,
+            @Param("billNo") String billNo,
+            @Param("beginTime") String beginTime,
+            @Param("endTime") String endTime,
+            @Param("organId") Long organId,
+            @Param("creator") Long creator,
+            @Param("handsPersonId") Long handsPersonId,
+            @Param("accountId") Long accountId,
+            @Param("status") String status,
+            @Param("remark") String remark,
+            @Param("number") String number);
+
     List<AccountHeadVo4ListEx> getDetailByNumber(
             @Param("billNo") String billNo);
 
