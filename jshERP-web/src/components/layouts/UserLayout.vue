8f28e7c2fe54d54de3621a414f5df1833dc5a55f
jishenghua <752718920@qq.com>
2025-11-24 21:54:44 +0800

    更新版本号为V3.6

--- jshERP-web/src/components/layouts/UserLayout.vue
+++ jshERP-web/src/components/layouts/UserLayout.vue
@@ -12,7 +12,7 @@
                 <a-col>
                   <a href="/">
                     <span class="title">{{systemTitle}}</span>
-                    <small class="desc">V3.5</small>
+                    <small class="desc">V3.6</small>
                   </a>
                 </a-col>
               </a-row>
