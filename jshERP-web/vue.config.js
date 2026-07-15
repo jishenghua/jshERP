b8c9a9a941b768e7c08f4652c276efc92759897e
jishenghua <752718920@qq.com>
2026-07-15 15:49:51 +0800

    解决开发环境无法断点调试的问题

--- jshERP-web/vue.config.js
+++ jshERP-web/vue.config.js
@@ -10,7 +10,10 @@ module.exports = {
     // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
     productionSourceMap: false,
     configureWebpack: config => {
-    // 生产环境取消 console.log
+        if (process.env.NODE_ENV !== 'production') {
+            config.devtool = 'source-map'
+        }
+        // 生产环境取消 console.log
         if (process.env.NODE_ENV === 'production') {
             config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
         }
